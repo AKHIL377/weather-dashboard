7eb0996aed1c796c46ead3b6d74c9cd805652ce0
Akhil <akisai223@gmail.com>
2025-05-04 21:06:35 +0530

    Initial commit

--- api/weather.js
+++ api/weather.js
@@ -1,10 +1,15 @@
 export default async function handler(req, res) {
-    const { city } = req.query;
-    const apiKey = process.env.WEATHER_KEY;
+  const { city } = req.query;
+  const apiKey = process.env.WEATHER_KEY;
+
+  try {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  
     const response = await fetch(url);
     const data = await response.json();
+
     res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  
+}
